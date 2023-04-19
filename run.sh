b78d409358f4551f8726363f0129cb647bc59f7f
freshines <freshines@gmail.com>
2023-04-19 19:27:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,6 @@
 
 warp-svc > /var/log/warp/log &
 (
-	//while ! warp-cli --accept-tos register; do
-	sleep 1
-	//>&2 echo "Awaiting warp-svc become online..."
-//done
 warp-cli --accept-tos set-mode proxy
 warp-cli --accept-tos set-proxy-port 40000
 warp-cli --accept-tos connect
